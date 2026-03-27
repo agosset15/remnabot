987109cc84909a116a1a30237aeef6eca9f31f80
agosset15 <ag@agosset.ru>
2026-03-27 21:16:32 +0300

    fix: ToggleLteSquad dishka import

--- src/application/use_cases/remnawave/__init__.py
+++ src/application/use_cases/remnawave/__init__.py
@@ -7,6 +7,7 @@ from .commands.management import (
     DeleteUserDevice,
     ReissueSubscription,
     ResetUserTraffic,
+    ToggleLteSquad,
 )
 from .commands.synchronization import SyncRemnaUser
 
@@ -16,4 +17,5 @@ REMNAWAVE_USE_CASES: Final[tuple[type[Interactor], ...]] = (
     DeleteUserAllDevices,
     ResetUserTraffic,
     ReissueSubscription,
+    ToggleLteSquad,
 )
