f5123aaec695cf81d9cb1820f8311673f269e5db
ag15 <ag@agosset.ru>
2026-09-18 12:30:57 +0300

    fix: dependencies

--- src/infrastructure/taskiq/tasks/update.py
+++ src/infrastructure/taskiq/tasks/update.py
@@ -13,7 +13,7 @@ from src.core.config import AppConfig
 from src.infrastructure.redis.keys import LatestNotifiedVersionKey
 from src.infrastructure.taskiq.broker import broker
 
-GITHUB_RELEASE_URL: Final[str] = "https://api.github.com/repos/snoups/remnashop/releases/latest"
+GITHUB_RELEASE_URL: Final[str] = "https://api.github.com/repos/agosset15/remnabot/releases/latest"
 
 
 @broker.task(schedule=[{"cron": "0 * * * *"}], retry_on_error=False)
